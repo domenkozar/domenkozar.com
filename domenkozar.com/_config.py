73949110ce9220b773a2537b01b7e5de44de48b5
Domen Kožar <domen@dev.si>
2011-04-30 22:22:11 +0200

    update config

--- domenkozar.com/_config.py
+++ domenkozar.com/_config.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 
+
+# needed to register sphinx directives in docutils rst
+import sphinx.directives
+import _rst_directive
+
 ######################################################################
 # This is your site's Blogofile configuration file.
 # www.Blogofile.com
@@ -19,7 +24,7 @@
 #  If you're hosting a blogofile powered site as a subdirectory of a larger
 #  non-blogofile site, then you would set the site_url to the full URL
 #  including that subdirectory: "http://www.yoursite.com/path/to/blogofile-dir"
-site.url = "http://www.yoursite.com"
+site.url = "http://www.domenkozar.com"
 
 #### Blog Settings ####
 blog = controllers.blog
@@ -38,11 +43,15 @@ blog.path = "/blog"
 
 ## blog_name -- Your Blog's name.
 # This is used repeatedly in default blog templates
-blog.name = "Your Blog's Name"
+blog.name = u"Domen Kožar"
 
 ## blog_description -- A short one line description of the blog
 # used in the RSS/Atom feeds.
-blog.description = "Your Blog's short description"
+blog.description = u"Domen Kožar's thoughts"
 
 ## blog_timezone -- the timezone that you normally write your blog posts from
-blog.timezone = "US/Eastern"
+blog.timezone = "Europe/Ljubljana"
+
+# disqus
+blog.disqus.enabled = True
+blog.disqus.name = "fubar"
